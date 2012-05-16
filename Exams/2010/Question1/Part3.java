d224e958336c2f499b1bda47c06c82365491c462
Ian Duffy <ian.duffy3@mail.dcu.ie>
2012-05-16 20:36:14 +0200

    Add spacing.

--- Exams/2010/Question1/Part3.java
+++ Exams/2010/Question1/Part3.java
@@ -10,13 +10,14 @@ class Part3 {
     }
 
     public static void main(String[] args) {
-        int[] [] b = {    {   3,   -4,   -1,   -3,   5,   4   },
+        int[] [] b = {
+            {   3,   -4,   -1,   -3,   5,   4   },
             {   4,   -8,    3,  10,  -31,  12   },
             {  13,  -11,   12,  16,   12,  11   },
         };
 
         int y=max(b, 2, 5);
 
-        System.out.println("The number of positive integers in the first 2 rows and 3 columns is "+y);
+        System.out.println("The number of positive integers in the first 2 rows and 5 columns is "+y);
     }
 }
